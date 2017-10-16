f3eae7ef28a4ee0f6cac42395aaeddf95245ae02
nayunhwan <nayunhwan.dev@gmail.com>
2017-10-16 15:06:52 +0900

    add one more Advanced test-case

--- tests/advanced_spec.js
+++ tests/advanced_spec.js
@@ -62,4 +62,22 @@ describe('advanced composer test', () => {
       { char: 't', style: { BOLD: true, ITALIC: false } },
     ])).toEqual('<p><b><i>b</b>te<b>st</i>t</b>e<b>st</b></p>');
   });
+
+  it('should return a proper paragraph with mixed text', () => {
+    expect(composer([
+      { char: 't', style: { BOLD: false, ITALIC: false } },
+      { char: 'e', style: { BOLD: false, ITALIC: false } },
+      { char: 's', style: { BOLD: false, ITALIC: false } },
+      { char: 't', style: { BOLD: false, ITALIC: false } },
+      { char: 'b', style: { BOLD: true, ITALIC: true } },
+      { char: 't', style: { BOLD: false, ITALIC: true } },
+      { char: 'e', style: { BOLD: false, ITALIC: true } },
+      { char: 's', style: { BOLD: true, ITALIC: true } },
+      { char: 't', style: { BOLD: true, ITALIC: true } },
+      { char: 't', style: { BOLD: true, ITALIC: false } },
+      { char: 'e', style: { BOLD: false, ITALIC: false } },
+      { char: 's', style: { BOLD: true, ITALIC: false } },
+      { char: 't', style: { BOLD: true, ITALIC: false } },
+    ])).toEqual('<p>test<b><i>b</b>te<b>st</i>t</b>e<b>st</b></p>');
+  });
 });
