0995ab2d068a78a34c9541597068eb798f0fdd9e
nayunhwan <nayunhwan.dev@gmail.com>
2017-10-16 22:28:32 +0900

    adjust logic

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ module.exports = function(styledString) {
   // WARNING: Do not use string.replace or regex operations.
 
   // Guard Condition for null or empty value
-  if (!styledString || styledString.length == 0) {
+  if (!styledString || styledString.length === 0) {
     return '<p></p>';
   }
 
@@ -40,29 +40,69 @@ module.exports = function(styledString) {
   let flagBold = false;
   let flagItalic = false;
 
+  const queue = [];
+
   styledString.forEach(item => {
-    if (flagItalic && flagItalic !== item.style.ITALIC) {
-      basedStr += '</i>';
-      flagItalic = false;
-    }
-    if (flagBold && flagBold !== item.style.BOLD) {
-      basedStr += '</b>';
-      flagBold = false;
-    }
+
+
     if (!flagBold && flagBold !== item.style.BOLD) {
-      basedStr += '<b>';
       flagBold = true;
     }
+
     if (!flagItalic && flagItalic !== item.style.ITALIC) {
-      basedStr += '<i>';
       flagItalic = true;
     }
-    basedStr += item.char;
+
+    if (flagItalic && flagItalic !== item.style.ITALIC) {
+      let tempStr = '';
+      while (queue.length !== 0) {
+        tempStr += queue.shift();
+      }
+      queue.push('<i>' + tempStr + '</i>');
+      flagItalic = false;
+    }
+
+    if (flagBold && flagBold !== item.style.BOLD) {
+      let tempStr = '';
+      while (queue.length !== 0) {
+        tempStr += queue.shift();
+      }
+      queue.push('<b>' + tempStr + '</b>');
+      flagBold = false;
+    }
+
+
+    if (flagBold || flagItalic) queue.push(item.char);
+    if (!flagBold && !flagItalic) {
+      while (queue.length !== 0) {
+        basedStr += queue.shift();
+      }
+      basedStr += item.char;
+    }
   });
 
   // Close tag
-  if (flagItalic) basedStr += '</i>';
-  if (flagBold) basedStr += '</b>';
+  // if (flagItalic) basedStr += '</i>';
+  // if (flagBold) basedStr += '</b>';
+  if (flagItalic) {
+    let tempStr = '';
+    while (queue.length !== 0) {
+      tempStr += queue.shift();
+    }
+    queue.push('<i>' + tempStr + '</i>');
+    flagItalic = false;
+  }
+
+  if (flagBold) {
+    let tempStr = '';
+    while (queue.length !== 0) {
+      tempStr += queue.shift();
+    }
+    queue.push('<b>' + tempStr + '</b>');
+    flagBold = false;
+  }
+
+  while (queue.length !== 0) basedStr += queue.shift();
   basedStr += '</p>';
 
   return basedStr; // FILL ME
